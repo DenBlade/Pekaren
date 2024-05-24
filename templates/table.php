e3118a489027c9caa6d32eecb3f14a8eaa61375b
DenBlade <denys.klinkov@gmail.com>
2024-05-24 22:58:56 +0300

    Header redirection problem was fixed

--- templates/table.php
+++ templates/table.php
@@ -1,9 +1,19 @@
 <?php
     include_once("./partials/header.php");
     if(!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] == 0){
-        // header("Location: 404.php");
+        header("Location: 404.php");
     }
     $table_name = $_GET['page'];
+    switch($table_name){
+        case "User":
+            $object = new User();
+            break;
+    }
+    if(isset($_POST['delete_contact'])) {
+        $object->delete($_POST['delete_contact']);
+        header("Location: table.php?page=$table_name");
+    }  
+    
 ?>
     <div class="container">
             <div class="row">
@@ -14,16 +24,7 @@
             <div class="row">
                 <div class="col-100">
                     <?php
-                    switch($table_name){
-                        case "User":
-                            $object = new User();
-                            break;
-                    }
                     $object->content_mapping();
-                    if(isset($_POST['delete_contact'])) {
-                        $object->delete($_POST['delete_contact']);
-                        // header("Location: table.php");
-                    }  
                     ?>
                 </div>
             </div>
